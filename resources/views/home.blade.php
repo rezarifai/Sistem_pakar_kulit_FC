7492e17fb7e7916c2840f2f1849024909a6e4892
reza <203200165@almaata.ac.id>
2024-07-05 00:28:57 +0700

    feat: DELETE Notif

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                             <a href="/penyakit" class="text-primary">Data Penyakit</a></div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jumlahPasien}}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jumlahPenyakit}}</div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
