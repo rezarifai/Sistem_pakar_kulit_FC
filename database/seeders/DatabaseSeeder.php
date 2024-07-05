552fe51f39bff99deae9d99f9a1dfe8252b83d7e
haaeell <haikaljawahir434@gmail.com>
2024-07-05 22:33:38 +0700

    fix: ruleseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(PenyakitSeeder::class);
-        $this->call(RuleSeeder::class);
         $this->call(GejalaSeeder::class);
+        $this->call(RuleSeeder::class);
     }
 }
